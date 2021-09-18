7bac007c7e43dc9c52f4f021dcfb635fc058706a
Per Pettersson <Per.Pettersson@gmail.com>
2021-09-18 08:44:33 -0400

    Shortcuts for window movements

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -11,6 +11,13 @@ M.setup = function()
   vim.api.nvim_set_keymap("n", "<TAB>", ":bnext<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("n", "<S-TAB>", ":bprevious<CR>", { noremap = true, silent = true })
 
+  -- Window Movement
+  vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
+
   vim.api.nvim_set_keymap('n', ';', ':', {noremap = true, silent = false})
   vim.api.nvim_set_keymap('v', ';', ':', {silent = false})
 
